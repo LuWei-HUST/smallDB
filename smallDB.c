591384b34e284081add6478acc1b200afc6d59be
luwei <1261579359@qq.com>
2025-07-13 22:25:17 +0800

    update

--- smallDB.c
+++ smallDB.c
@@ -188,6 +188,7 @@ void indent(uint32_t level);
 void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level);
 uint32_t get_unused_page_num(Pager* pager);
 void create_new_root(Table* table, uint32_t right_child_page_num);
+Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key);
 
 
 void print_row(Row* row) {
@@ -549,8 +550,7 @@ Cursor* table_find(Table* table, uint32_t key) {
   if (get_node_type(root_node) == NODE_LEAF) {
     return leaf_node_find(table, root_page_num, key);
   } else {
-    printf("Need to implement searching an internal node\n");
-    exit(EXIT_FAILURE);
+    return internal_node_find(table, root_page_num, key);
   }
 }
 
@@ -820,6 +820,33 @@ void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level) {
       child = *internal_node_right_child(node);
       print_tree(pager, child, indentation_level + 1);
       break;
+  }
+}
+
+Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key) {
+  void* node = get_page(table->pager, page_num);
+  uint32_t num_keys = *internal_node_num_keys(node);
+
+  /* Binary search to find index of child to search */
+  uint32_t min_index = 0;
+  uint32_t max_index = num_keys; /* there is one more child than key */
+
+  while (min_index != max_index) {
+    uint32_t index = (min_index + max_index) / 2;
+    uint32_t key_to_right = *internal_node_key(node, index);
+    if (key_to_right >= key) {
+      max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
+  uint32_t child_num = *internal_node_child(node, min_index);
+  void* child = get_page(table->pager, child_num);
+  switch (get_node_type(child)) {
+    case NODE_LEAF:
+      return leaf_node_find(table, child_num, key);
+    case NODE_INTERNAL:
+      return internal_node_find(table, child_num, key);
   }
 }
 
